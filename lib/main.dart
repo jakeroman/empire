d89658c70c31a28cf281f31bda04b1b1940c0c1f
jakemroman <jakemroman@gmail.com>
2024-02-05 15:06:07 -0500

    Refactored to use firebase_core.dart, fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Firebase Imports
-import 'package:firebase_core_dart/firebase_core_dart.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -24,10 +24,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Temporary auth printout
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+
+    // Return relevant page
     return const MaterialApp(
       home: SplashScreen(),
     );
   }
+
   // return MaterialApp(
   //   title: 'Flutter Demo',
   //   theme: ThemeData(
