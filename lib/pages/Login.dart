aa3c3de493b15324c5a21822e249f4d0dfc60625
Mitch Brown <biscuitbob13337@gmail.com>
2024-02-15 17:41:28 -0500

    Added splash_screen

---
+++ lib/pages/Login.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Container(
+        color: Colors.red, // Set the background color to red
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle login logic here
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,13 +1,72 @@
+import 'package:empire/pages/Login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:empire/components/button.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+//Switching to homepage
+
+class _SplashScreenState extends State<SplashScreen> {
+  // @override
+  // void initstate() {
+  //   super.initState();
+  //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  //   Future.delayed(const Duration(seconds: 5), () {
+  //     Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(
+  //         builder: (_) => const LoginPage(),
+  //       ),
+  //     );
+  //   });
+  // }
+
+  //ADD BUTTON TO GO TO LOGIN PAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('Splash Screen!'),
-    ));
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.red, Colors.yellow],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // child: RaisedButton(
+          //   child: Text("Enter Arena"),
+          //   Color(Colors.black),
+          //   onPressed(){
+          //     Navigator.push(context, MertialPageRoute(builder: (context)=>LoginPage()));
+          //   }
+          // )
+          children: [
+            Icon(
+              Icons.account_box,
+              size: 75,
+              color: Colors.black,
+            ),
+            SizedBox(height: 25),
+            Text(
+              "Welcome to Empire",
+              style: TextStyle(
+                fontStyle: FontStyle.normal,
+                color: Colors.black,
+                fontSize: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
